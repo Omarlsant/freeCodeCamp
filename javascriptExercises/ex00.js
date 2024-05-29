0382d6d0e31761c39421134f768e262005b958b2
Omar Lengua <omarns21@gmail.com>
2024-05-29 14:34:38 +0200

    agregados ejercicios de lógica, hasta el step 24

--- javascriptExercises/ex00.js
+++ javascriptExercises/ex00.js
@@ -1,3 +1,10 @@
 let character = 'Hello';
 let count = 8;
-console.log(count + 1);
+
+let rows = ["Naomi", "Quincy", "CamperChan"];
+let popped;
+
+let pushed;
+pushed = rows.push("freeCodeCamp");
+
+console.log(pushed  );
